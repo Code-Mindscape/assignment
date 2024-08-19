fbce3af81d702e559df29201f7705cef536a77af
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-08-20 00:17:45 +0500

    deleted fn

--- CountVowels.cpp
+++ CountVowels.cpp
@@ -1,17 +1,6 @@
 #include<iostream>
 using namespace std;
 
-int countVow(char str[], int size){
-    int count = 0;
-    for (int i = 0; i < size; i++)
-    {
-        count++;
-    }
-
-    return count;
-    
-    
-}
   
 int main(){
 
