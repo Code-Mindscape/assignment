a6f89e2ae30cba98c05d889660b727e9179ab6c2
Mirza Usman <mirzausmanmu07842@gmail.com>
2024-08-20 00:22:20 +0500

    Added fn

--- CountVowels.cpp
+++ CountVowels.cpp
@@ -1,6 +1,17 @@
 #include<iostream>
 using namespace std;
 
+int countVow(char str[], int size){
+    int count = 0;
+    for (int i = 0; i < size; i++)
+    {
+        count++;
+    }
+
+    return count;
+    
+    
+}
   
 int main(){
 
